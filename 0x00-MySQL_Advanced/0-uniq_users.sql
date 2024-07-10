0c265bed6fac49000ad7d2a3e033f6818191c7f2
Michael Iyke <ymichaelc@gmail.com>
2024-07-11 00:04:24 +0100

    Task 00: create a unique user

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,2 +1,8 @@
 -- Advanced SQL Lesson 0: Unique Users
--- In this lesson, we will learn how to count the number of unique users.
+-- In this lesson, we will learn how to create a table with unique users.
+USE holberton;
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+);
