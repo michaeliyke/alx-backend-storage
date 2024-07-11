52785b143998d1e9354e4d72cc4e15472ed1a40b
Michael Iyke <ymichaelc@gmail.com>
2024-07-11 21:00:23 +0100

    Task 02: count fans of each band and group by origin

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,2 +1,8 @@
 -- Advanced SQL Lesson 2: Fans
--- In this lesson, we will learn how to count the number of fans for each artist.
+-- In this lesson, we  rank country origins of bands, ordered by the number of (non-unique) fans
+-- This merely counts the number of fans for each band, and then groups them by origin
+SELECT origin,
+	SUM(nb_fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
