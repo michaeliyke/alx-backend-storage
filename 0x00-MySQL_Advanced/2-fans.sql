2344ccd1edcbf6a828e93fd6abf6c8ea44c73ea9
Michael Iyke <ymichaelc@gmail.com>
2024-07-11 21:06:31 +0100

    Correction

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -2,7 +2,8 @@
 -- In this lesson, we  rank country origins of bands, ordered by the number of (non-unique) fans
 -- This merely counts the number of fans for each band, and then groups them by origin
 SELECT origin,
-	SUM(nb_fans) AS nb_fans
+	-- SUM(nb_fans) AS nb_fans
+	COUNT(*) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
