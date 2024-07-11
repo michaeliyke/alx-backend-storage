5546a534237ccf20dd66fc8402fb6217be5526cc
Michael Iyke <ymichaelc@gmail.com>
2024-07-11 19:22:59 +0100

    Task 01: Define the enum type in a table

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,2 +1,8 @@
 -- Advanced SQL Lesson 1: Country Users
--- In this lesson, we will learn how to count the number of users from each country.
+-- In this lesson, we will learn create Users table with enum column country.
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') NOT NULL
+);
