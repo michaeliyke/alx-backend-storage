9bec71b562e547598db0e7c66d1a101d455bd928
Michael Iyke <ymichaelc@gmail.com>
2024-07-12 11:44:32 +0100

    Task 03: Correction

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,8 +1,8 @@
 -- Advanced SQL Lesson 3: Glam Rock
 -- Lists all bands with Glam rock as their main style, ranked by their longevity
--- Note, the default split year is 2020 in case it is NULL
+-- Note, the default split year is 2022 in case it is NULL
 SELECT band_name,
-	ABS(COALESCE(split, 2020) - formed) AS lifespan
+	ABS(COALESCE(split, 2022) - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC,
