318c5cb55898fa940732f56d205fdf2c71a0c130
Michael Iyke <ymichaelc@gmail.com>
2024-07-12 21:26:26 +0100

    Task 07: Stored routine (Stored procedure) to calculate score avarage

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,2 +1,30 @@
 -- Advanced SQL Lesson 7: Average Score
--- In this lesson, we will learn how to calculate the average score for each album.
+--  creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student
+-- Note: An average score can be a decimal
+
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN  -- Multi-statement block starts here. Not necessary for single-statement blocks.
+
+	-- Declare variables
+	DECLARE avg_score FLOAT;
+	DECLARE total_score FLOAT;
+	DECLARE total_projects INT;
+
+	SELECT SUM(score), COUNT(*) INTO total_score, total_projects FROM corrections WHERE user_id = user_id;
+
+	-- Calculate average score
+	IF total_projects = 0 THEN
+		SET avg_score = 0;
+	ELSE
+		SET avg_score = total_score / total_projects;
+	END IF;
+
+	-- Update the user's average score in the users table
+	UPDATE users SET average_score = avg_score WHERE id = user_id;
+END//
+
+DELIMITER ;
