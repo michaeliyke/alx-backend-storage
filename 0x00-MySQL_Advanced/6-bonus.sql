104f43efbacc26f54381cbc68fc9d3846fe7f05e
Michael Iyke <ymichaelc@gmail.com>
2024-07-12 21:03:38 +0100

    Task 06: Stored routine (stored procedure)

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,2 +1,23 @@
 -- Advanced SQL Lesson 6: Bonus
--- In this lesson, we will learn how to count the number of users who have purchased all of an artist's albums.
+-- script that creates a stored procedure AddBonus that adds a new correction for a student.
+
+DROP PROCEDURE IF EXISTS AddBonus;
+
+DELIMITER //
+
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN u_score INT)
+BEGIN  -- Multi-statement block starts here. Not necessary for single-statement blocks.
+	DECLARE proj_id INT;
+
+	SELECT id INTO proj_id FROM projects WHERE name = project_name;
+
+	IF proj_id IS NULL THEN
+		INSERT INTO projects (name) VALUES (project_name);
+		SET proj_id = LAST_INSERT_ID();
+	END IF;
+
+	INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, proj_id, u_score);
+
+END//
+
+DELIMITER ;
