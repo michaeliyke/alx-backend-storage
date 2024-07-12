a60a9950ab1cb905eee7739b9a722e17ddb0a9a3
Michael Iyke <ymichaelc@gmail.com>
2024-07-12 11:44:32 +0100

    Task 03: Correction

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,8 +1,9 @@
 -- Advanced SQL Lesson 3: Glam Rock
 -- Lists all bands with Glam rock as their main style, ranked by their longevity
+-- Note, the default split year is 2020 in case it is NULL
 SELECT band_name,
-	ABS(split - formed) AS lifespan
+	ABS(COALESCE(split, 2020) - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
-	AND split <= 2022
-ORDER BY lifespan DESC;
+ORDER BY lifespan DESC,
+	band_name DESC;
--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,2 +1,9 @@
 -- Advanced SQL Lesson 4: Store
--- In this lesson, we will learn how to count the number of purchases for each album.
+-- creates a trigger that decreases the quantity of an item after adding a new order.
+CREATE TRIGGER decrease_item_quantity
+AFTER
+INSERT ON orders FOR EACH ROW BEGIN
+UPDATE store
+SET quantity = quantity - NEW.quantity
+WHERE item = NEW.item;
+END;
