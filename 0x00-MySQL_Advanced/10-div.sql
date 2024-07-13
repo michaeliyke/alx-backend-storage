16351d451d656e0b490931d5737f318883ef4d95
Michael Iyke <ymichaelc@gmail.com>
2024-07-13 13:37:58 +0100

    Task 10: Function that divides one argument with the other

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,2 +1,20 @@
 -- Advanced SQL Lesson 10: Div
--- In this lesson, we will learn how to divide two numbers.
+-- function SafeDiv that divides & returns the 1st by the 2nd number.
+-- Returns 0 if the second number is equal to 0
+
+DROP FUNCTION IF EXISTS SafeDiv;
+
+DELIMITER //
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS INT
+BEGIN
+	IF b = 0 THEN
+		RETURN 0;
+	ELSE
+		RETURN a / b;
+	END IF;
+END//
+
+DELIMITER ;
+
