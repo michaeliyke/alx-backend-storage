7f541eca8cfeaeacd9d061e541370738f75cc16e
Michael Iyke <ymichaelc@gmail.com>
2024-07-13 12:56:11 +0100

    Task 09: Create an index for first char of name column and score column

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,2 +1,9 @@
 -- Advanced SQL Lesson 9: Index Name Score
--- In this lesson, we will learn how to create an index on the name column and calculate the score for each album.
+-- creates an index idx_name_first_score on the table names and the
+-- first letter of name and the score
+
+-- DROP INDEX IF EXISTS idx_name_first_score;
+
+-- CREATE INDEX idx_name_first_score ON names (user_id, LEFT((SELECT name FROM users WHERE id = user_id), 1), score);
+
+CREATE INDEX idx_name_first_score ON names (name(1), score);
