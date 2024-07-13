5db526e6ebc2ed1d54265bf21312ca7a41abf65b
Michael Iyke <ymichaelc@gmail.com>
2024-07-13 15:46:16 +0100

    Task 11: A view for students who need a meeting

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,2 +1,22 @@
 -- Advanced SQL Lesson 11: Need Meeting
--- In this lesson, we will learn how to count the number of users who need a meeting.
+-- creates a view need_meeting that lists all students that have a score under 80 (strict)
+--  and no last_meeting or more than 1 month
+
+-- A view in SQL is a virtual table based on the result set of a SQL query.
+-- A view contains rows and columns, just like a real table.
+-- The fields in a view are fields from one or more real tables in the database.
+-- It doesn't store the data itself but presents data stored in tables in a specific,
+-- often simplified, way.
+
+-- Views can be used to:
+--
+-- Simplify complex queries.
+-- Provide a layer of abstraction.
+-- Enhance security by restricting access to specific data.
+-- Present data in a specific format or structure.
+
+CREATE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE score < 80
+AND (last_meeting IS NULL OR last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH));
