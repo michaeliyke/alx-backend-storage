ef4e07dbe7a2cb8af5d5720e0daf200b79c43ea2
Michael Iyke <ymichaelc@gmail.com>
2024-07-13 23:51:04 +0100

    Task 101: Procedure to peform weighted average for all users

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,2 +1,62 @@
 -- Advanced SQL Lesson 101: Average Weighted Score
--- In this lesson, we will learn how to calculate the average weighted score for each album.
+-- creates a stored procedure ComputeAverageWeightedScoreForUsers that computes and store the average weighted score for all students.
+
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
+
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
+
+BEGIN
+	-- Declare variables
+	DECLARE weighted_total_score FLOAT;
+	DECLARE total_weight FLOAT;
+	DECLARE average_score FLOAT;
+	DECLARE user_id INT;
+	DECLARE done INT DEFAULT FALSE;
+	DECLARE cur CURSOR FOR SELECT id FROM users;
+	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+	-- Open the cursor
+	OPEN cur;
+
+	-- Loop through all users
+	repeat
+		FETCH cur INTO user_id;
+		IF NOT done THEN
+			-- Calculate the sum of weighted scores for the user
+			SELECT SUM(corrections.score * projects.weight) INTO weighted_total_score
+			FROM corrections
+			INNER JOIN projects
+			ON corrections.project_id = projects.id
+			WHERE corrections.user_id = user_id;
+
+			-- Calculate the total weight of projects for the user
+			SELECT SUM(projects.weight) INTO total_weight
+			FROM corrections
+			INNER JOIN projects
+			ON corrections.project_id = projects.id
+			WHERE corrections.user_id = user_id;
+
+			-- Compute the average weighted score finally, handle division by zero
+			IF total_weight > 0 THEN
+				SET average_score = ABS(weighted_total_score / total_weight);
+			ELSE
+				SET average_score = 0;
+			END IF;
+
+			-- Update the user's average score in the users table
+			UPDATE users
+			SET average_score = average_score
+			WHERE id = user_id;
+		END IF;
+	until done END REPEAT;
+
+	-- Close the cursor
+	CLOSE cur;
+
+END//
+
+DELIMITER ;
+
+-- DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
--- 0x00-MySQL_Advanced/main.sql
+++ 0x00-MySQL_Advanced/main.sql
@@ -1,13 +1,9 @@
 -- Show and compute average weighted score
-SELECT "--";
 SELECT * FROM users;
-SELECT "--";
 SELECT * FROM projects;
-SELECT "--";
 SELECT * FROM corrections;
-SELECT "--";
 
-CALL ComputeAverageWeightedScoreForUser((SELECT id FROM users WHERE name = "Jeanne"));
+CALL ComputeAverageWeightedScoreForUsers();
 
 SELECT "--";
 SELECT * FROM users;
