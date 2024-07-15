209c038ca0a51c14992ed36b5c72dba14c1bbc5c
Michael Iyke <ymichaelc@gmail.com>
2024-07-15 10:57:11 +0100

    Commenting more clearly

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -13,16 +13,24 @@ BEGIN
 	DECLARE total_weight FLOAT;
 	DECLARE average_score FLOAT;
 	DECLARE user_id INT;
+	-- Flag var to control loop termination.
+	-- TRUE indicates no more users to process. (INT initialized to FALSE)
 	DECLARE done INT DEFAULT FALSE;
+	-- Define a cursor to fetch user IDs one by one from the users table
 	DECLARE cur CURSOR FOR SELECT id FROM users;
+	-- Define a special handler for the cursor above
+	-- Defines what action to take when the cursor reaches the end of the data
+	-- Here sets the done variable to TRUE when the NOT FOUND condition occurs.
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
 
-	-- Open the cursor
+	-- Open the cursor ready to retrieve data
 	OPEN cur;
 
-	-- Loop through all users
+	-- Loop continues iterating as long as the done flag is FALSE
 	repeat
+		-- Retrieve next user ID from the cursor, store in user_id
 		FETCH cur INTO user_id;
+		-- Check if the done flag is still FALSE
 		IF NOT done THEN
 			-- Calculate the sum of weighted scores for the user
 			SELECT SUM(corrections.score * projects.weight) INTO weighted_total_score
