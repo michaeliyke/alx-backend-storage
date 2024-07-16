48cb000b26cee48c89d884b5612dd0594f74b9ca
Michael Iyke <ymichaelc@gmail.com>
2024-07-16 13:37:00 +0100

    Task 11: List schools by topic they offer

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,2 +1,7 @@
 #!/usr/bin/env python3
-# Function that returns the list of school having a specific topic
+"""Function that returns the list of school having a specific topic"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """Return the list of school having a specific topic"""
+    return mongo_collection.find({"topics": topic})
