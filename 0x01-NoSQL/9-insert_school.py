638ed5fd64eea79cf0bc679cfda2999b1d8d6ccc
Michael Iyke <ymichaelc@gmail.com>
2024-07-16 11:38:13 +0100

    Task 09: insert a document with pymongo driver

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,2 +1,7 @@
 #!/usr/bin/env python3
-# Function that inserts a new document in a collection based on kwargs
+"""Function that inserts a new document in a collection based on kwargs"""
+
+
+def insert_school(mongo_collection, **kwargs) -> str:
+    """Inserts a new document in a collection based on kwargs"""
+    return mongo_collection.insert_one(kwargs).inserted_id
