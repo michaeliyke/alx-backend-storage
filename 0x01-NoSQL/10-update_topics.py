b200eb5aee99c1b7c4b7e60f7ed29f60871c09e3
Michael Iyke <ymichaelc@gmail.com>
2024-07-16 11:58:11 +0100

    Task 10: update many

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 """Function that changes all topics of a school document based on the name"""
-from pymongo.database import Collection
-from typing import List
 
 
-def update_topics(mongo_collection: Collection, name: str, topics: List[str]):
+def update_topics(mongo_collection, name, topics):
     """Changes all topics of a school document based on the name"""
     mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
     return
--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """Function that lists all documents in a collection"""
-from pymongo.collection import Collection
 
 
-def list_all(mongo_collection: Collection):
+def list_all(mongo_collection):
     """Lists all documents in a collection"""
     return mongo_collection.find()
