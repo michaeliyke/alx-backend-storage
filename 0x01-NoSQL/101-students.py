8edfbbd15abf04c06676e7a48f9b8ba0d1861548
Michael Iyke <ymichaelc@gmail.com>
2024-07-16 20:41:36 +0100

    Task 101: Sort a list of documents

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,2 +1,21 @@
 #!/usr/bin/env python3
-# Function that returns all students sorted by average score
+"""Function that returns all students sorted by average score"""
+
+
+def top_students(mongo_collection):
+    """List all students sorted by average score"""
+    return mongo_collection.aggregate([
+        {
+            '$project': {
+                'name': '$name',
+                'averageScore': {
+                    '$avg': '$topics.score'
+                }
+            }
+        },
+        {
+            '$sort': {
+                'averageScore': -1
+            }
+        }
+    ])
