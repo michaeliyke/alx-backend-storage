62de23b9dff6aeabd4684e79755e4acf9ef99ae8
Michael Iyke <ymichaelc@gmail.com>
2024-07-16 16:58:11 +0100

    Task 12: importing a BSON using mongorestore and querying data stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,2 +1,25 @@
 #!/usr/bin/env python3
-# Provides some stats about Nginx logs stored in MongoDB
+"""Provides some stats about Nginx logs stored in MongoDB"""
+
+
+def log_stats(nginx):
+    """Provides some stats about Nginx logs stored in MongoDB"""
+    print(f"{nginx.count_documents({})} logs")
+    print("Methods:")
+    print(
+        f"\tmethod GET: {nginx.count_documents({'method': 'GET'})}")
+    print(
+        f"\tmethod POST: {nginx.count_documents({'method': 'POST'})}")
+    print(
+        f"\tmethod PUT: {nginx.count_documents({'method': 'PUT'})}")
+    print(
+        f"\tmethod PATCH: {nginx.count_documents({'method': 'PATCH'})}")
+    print(
+        f"\tmethod DELETE: {nginx.count_documents({'method': 'DELETE'})}")
+    print(f"{nginx.count_documents({'method': 'GET', 'path': '/status'})} status check")
+
+
+if __name__ == "__main__":
+    from pymongo import MongoClient
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    log_stats(client.logs.nginx)
--- 0x01-NoSQL/main.py
+++ 0x01-NoSQL/main.py
@@ -1,26 +1,9 @@
 #!/usr/bin/env python3
-""" 11-main """
-from pymongo import MongoClient
-list_all = __import__('8-all').list_all
-insert_school = __import__('9-insert_school').insert_school
-schools_by_topic = __import__('11-schools_by_topic').schools_by_topic
+""" 12-main """
+
+log_stats = __import__('12-log_stats').log_stats
 
 if __name__ == "__main__":
+    from pymongo import MongoClient
     client = MongoClient('mongodb://127.0.0.1:27017')
-    school_collection = client.my_db.school
-
-    j_schools = [
-        {'name': "Holberton school", 'topics': [
-            "Algo", "C", "Python", "React"]},
-        {'name': "UCSF", 'topics': ["Algo", "MongoDB"]},
-        {'name': "UCLA", 'topics': ["C", "Python"]},
-        {'name': "UCSD", 'topics': ["Cassandra"]},
-        {'name': "Stanford", 'topics': ["C", "React", "Javascript"]}
-    ]
-    for j_school in j_schools:
-        insert_school(school_collection, **j_school)
-
-    schools = schools_by_topic(school_collection, "Python")
-    for school in schools:
-        print("[{}] {} {}".format(school.get('_id'),
-              school.get('name'), school.get('topics', "")))
+    log_stats(client.logs.nginx)
