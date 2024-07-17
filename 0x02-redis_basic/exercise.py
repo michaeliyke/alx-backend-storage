c4d96d895073a839c943dca12b6ff4858cb4d6bd
Michael Iyke <ymichaelc@gmail.com>
2024-07-17 22:01:08 +0100

    Task 01: create a get() methods

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -13,9 +13,30 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, float, int, bytes]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """Generate a random key, store the data in Redis using the key
         and return the key"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: callable = None):
+        """Get the data from the key"""
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """
+        Calling this method will call self.get()
+        and supply the correct conversion function for a string
+        """
+        return self.get(key=key, fn=lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """
+        Calling this method will call self.get()
+        and suply the correct conversion function for an integer
+        """
+        return self.get(key=key, fn=int)
