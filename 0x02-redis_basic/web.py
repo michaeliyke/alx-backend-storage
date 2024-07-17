9534e69cf2a534d0658327207440b4aa31c88457
Michael Iyke <ymichaelc@gmail.com>
2024-07-18 00:28:20 +0100

    Correction

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,16 +4,16 @@
 from typing import Callable
 import functools
 import redis
+import redis.exceptions
 import requests
+
+conn = redis.Redis()
 
 
 def cache_results(method: Callable[[str], str]) -> Callable:
     """cache the result with an expiration time of 10 seconds"""
     @functools.wraps(method)
     def wrapper(url: str) -> str:
-        """Wrapper function to be returned"""
-        conn = redis.Redis()
-
         if conn.get(url):  # Check if the cache exists
             return conn.get(url).decode('utf-8')
         html = method(url)
@@ -26,8 +26,6 @@ def track_url(method: Callable[[str], str]) -> Callable:
     """Track the number of times a URL is called"""
     @functools.wraps(method)
     def wrapper(url: str) -> str:
-        """Wrapper function to be returned"""
-        conn = redis.Redis()
         key = f"count:{url}"
         conn.incr(key)
         return method(url)
@@ -38,11 +36,19 @@ def track_url(method: Callable[[str], str]) -> Callable:
 @cache_results
 def get_page(url: str) -> str:
     """Get the content of a web page"""
-    return requests.get(url).text
+    response = requests.get(url)
+    response.raise_for_status()  # Raise an exception for 4xx and 5xx errors
+    return response.text
 
 
 if __name__ == '__main__':
     url = "http://slowwly.robertomurray.co.uk"
-    url = "https://google.com"
-    print(get_page(url))
-    print(get_page(url))
+    url = "https:// google.com"
+
+    try:
+        print(get_page(url))
+        print(get_page(url))
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching the URL: {e}")
+    except redis.exceptions.ConnectionError as e:
+        print(f"Redis error: {e}")
