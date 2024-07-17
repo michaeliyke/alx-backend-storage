810fe0e481c8b548c581e09d9ce97d7dbbda80e0
Michael Iyke <ymichaelc@gmail.com>
2024-07-17 21:34:45 +0100

    Task 00: A Redis Cache class with store method

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+"""0. Writing strings to Redis"""
+import redis
+import uuid
+
+
+class Cache:
+    """0. Writing strings to Redis"""
+
+    def __init__(self):
+        """The redis cache constructor"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: bytes) -> str:
+        """Generate a random key, store the data in Redis using the key
+        and return the key"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
